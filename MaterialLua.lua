3d2f03ced26ddb83f4865a7c6f1d8768625f9050
Unknown <69674484+Unknown-publisher@users.noreply.github.com>
2021-04-14 19:06:26 +0200

    bruh

--- MaterialLua.lua
+++ MaterialLua.lua
@@ -921,7 +921,7 @@ function Material.Load(Config)
 		BannerOverlay.ZIndex = 75
 		BannerOverlay.Parent = MainFrame
 
-		local TextSize = TextService:GetTextSize(BannerText, 12, Enum.Font.Gotham, Vector2.new(0,0)).X
+		local TextSize = TextService:GetTextSize(BannerText, 12, Enum.Font.FredokaOne, Vector2.new(0,0)).X
 
 		local Lines = math.ceil((TextSize) / (MainFrame.AbsoluteSize.X - 10))
 
@@ -942,7 +942,7 @@ function Material.Load(Config)
 		BannerLabel.Text = BannerText
 		BannerLabel.TextColor3 = Theme.BannerAccent
 		BannerLabel.TextSize = 12
-		BannerLabel.Font = Enum.Font.Gotham
+		BannerLabel.Font = Enum.Font.FredokaOne
 		BannerLabel.Size = UDim2.fromScale(1,0) + UDim2.fromOffset(-5,(Lines*20)+5)
 		BannerLabel.TextWrapped = true
 		BannerLabel.Position = UDim2.fromOffset(5,0)
