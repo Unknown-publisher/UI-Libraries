1541cd7b06097e8b3a3c6e3270010cd96f32079f
Unknown <69674484+Unknown-publisher@users.noreply.github.com>
2021-09-18 10:51:14 +0200

    Update BracketV3.lua

--- BracketV3.lua
+++ BracketV3.lua
@@ -744,11 +744,6 @@ function Library:CreateWindow(Config, Parent)
 					Callback(Color)
 				end
 				
-				if ColorDefault == nil then
-				else
-					Callback(ColorDefault)
-				end
-				
 				Pallete.Input.InputBox.FocusLost:Connect(function(Enter)
 					if Enter then
 						local ColorString = string.split(string.gsub(Pallete.Input.InputBox.Text," ", ""), ",")
